438815b10442c575036e8209242fab25bdaf1c27
강 <ghang0501@gmail.com>
2023-06-21 20:37:19 +0900

    4.2 Test Drive part Twe
    강의 4,2에서는 프로필 카드를 만들었다

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,28 +3,49 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-5 ">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="grid gap-5 bg-slate-400 px-10 py-20 ">
+      <div className="rounded-3xl bg-white p-6 shadow-xl">
+        <span className="text-3xl font-semibold">Select Item</span>
         <div className="flex justify-between">
-          <span className="text-gray-500 my-1">Gray Chair</span>
+          <span className="my-1 text-gray-500">Gray Chair</span>
           <span className="font-semibold">$19</span>
         </div>
         <div className="flex justify-between">
-          <span className="text-gray-500 my-1">Gray Chair</span>
+          <span className="my-1 text-gray-500">Gray Chair</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span className="text-gray-500">Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+        <div className="mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-3 text-center text-white">
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="overflow-hidden rounded-2xl bg-white  shadow-xl">
+        <div className="bg-blue-500 p-6 pb-5">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative -top-4 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative   -mt-10 flex flex-col items-center">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-400">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
     </div>
   );
 };
