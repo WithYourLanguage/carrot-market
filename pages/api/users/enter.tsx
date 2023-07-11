6125bc4d8c0353f9186c286bc9b0092b3a0accee
강 <ghang0501@gmail.com>
2023-07-11 15:02:40 +0900

    9.9

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
@@ -10,9 +12,8 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : email ? { email } : null;
+  const user = phone ? { phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
-
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -23,23 +24,33 @@ async function handler(
             ...user,
           },
           create: {
-            ...user,
             name: "Anonymous",
+            ...user,
           },
         },
       },
     },
   });
   if (phone) {
-    const message = await twilioClient.messages.create({
+    /*  const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.PHONE!,
-      body: `Your Login Token is ${payload}`,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}.`,
     });
-    console.log(message);
+    console.log(message); */
+  } else if (email) {
+    /* const email = await mail.send({
+      from: "nico@nomadcoders.co",
+      to: "nico@nomadcoders.co",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email); */
   }
   return res.json({
     ok: true,
   });
 }
+
 export default withHandler("POST", handler);
