2db467d0d2cfb23875a95408cb79cecd4d726c1f
강 <ghang0501@gmail.com>
2023-07-13 21:25:50 +0900

    11.3 See Products

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -11,27 +11,36 @@ async function handler(
     body: { name, price, description },
     session: { user },
   } = req;
-  const product = await client.product.create({
-    data: {
-      name,
-      price: +price,
-      description,
-      image: "xx",
-      user: {
-        connect: { id: user?.id },
+  if (req.method === "GET") {
+    const products = await client.product.findMany({});
+    res.json({
+      ok: true,
+      products,
+    });
+  }
+  if (req.method === "POST") {
+    const product = await client.product.create({
+      data: {
+        name,
+        price: +price,
+        description,
+        image: "xx",
+        user: {
+          connect: { id: user?.id },
+        },
       },
-    },
-  });
-  res.json({
-    ok: true,
+    });
+    res.json({
+      ok: true,
 
-    product,
-  });
+      product,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    method: "POST",
+    methods: ["GET", "POST"],
     handler,
   })
 );
